a21111bf6d0beeb0ff1d0a4b2f14f2a947fc0628
GumBrew <zchuanqi045@gmail.com>
2021-11-06 21:45:14 +0900

    pushメッセージ対応

--- deployPrj01/LineHttpTriggeredFunction/index.js
+++ deployPrj01/LineHttpTriggeredFunction/index.js
@@ -53,8 +53,8 @@ const client = new line.Client(config);
 // kawa:Bさん接続用LINEオブジェクトを生成
 //const client2 = new line.Client(config2);
 
-const token = 'Yko44X8qrMuYUTTxy3cBisv3AmDAtsK+HuMriKEuX2U40EOnbHZRY2iWyEp8IY5Rlcjt+Xnz7HFou5waZ5Hq3duS1e9938pM5RMHJQ04jWLBaY3TtBDQCkprW8G3vuEFaLsHKejvPeRHknpETPnFCAdB04t89/1O/w1cDnyilFU'
-const userId = 'U5e7442aa88d9ad061a14761447955f78'
+const token = 'Yko44X8qrMuYUTTxy3cBisv3AmDAtsK+HuMriKEuX2U40EOnbHZRY2iWyEp8IY5Rlcjt+Xnz7HFou5waZ5Hq3duS1e9938pM5RMHJQ04jWLBaY3TtBDQCkprW8G3vuEFaLsHKejvPeRHknpETPnFCAdB04t89/1O/w1cDnyilFU';
+const userId = 'U5e7442aa88d9ad061a14761447955f78';
 
 const client2 = new line.Client({
   channelAccessToken: token
